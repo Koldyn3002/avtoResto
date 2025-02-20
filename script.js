d3a7bfefd57520c6a88b301d5d6b71c807438eda
Koldyn3002 <vladd3002@gmail.com>
2025-02-20 19:13:13 +0300

    Улучшена организация CSS стилей

--- script.js
+++ script.js
@@ -47,3 +47,17 @@ logosContainer.addEventListener("mousemove", (e) => {
   const walk = (x - startPosition) * 2; // Множитель скорости
   logosContainer.scrollLeft = scrollLeft - walk;
 });
+
+// Добавляем обработчик скролла
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  const address = document.querySelector('.address');
+  
+  if (window.scrollY > 100) { // Когда проскроллили больше 100px
+    header.classList.add('header-scrolled');
+    address.classList.add('address-hidden');
+  } else {
+    header.classList.remove('header-scrolled');
+    address.classList.remove('address-hidden');
+  }
+});
